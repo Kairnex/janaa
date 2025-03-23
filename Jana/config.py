486054309fe78ae45d51fe79ce28ea52226c7549
Kairnex <codexkairnex@gmail.com>
2025-03-23 12:48:22 +0530

    Update config.py

--- Jana/config.py
+++ Jana/config.py
@@ -1,20 +1,20 @@
 class Config(object):
     LOGGER = True
-    API_ID =None 
-    API_HASH = ""
+    API_ID =21189715 
+    API_HASH = "988a9111105fd2f0c5e21c2c2449edfd"
     TOKEN = ""  
-    OWNER_ID=None
+    OWNER_ID=7225660023
     
-    SUPPORT_CHAT = "" 
-    START_IMG = ""
-    EVENT_LOGS = ()
+    SUPPORT_CHAT = "Friend_Chat_International" 
+    START_IMG = "https://files.catbox.moe/tre76f.jpg"
+    EVENT_LOGS = (-1002341427688)
     MONGO_DB_URI= ""
    
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""
+        "8SOSKTRI0KPL"
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "https://timezonedb.com/api"
 
     
     BL_CHATS = [] 
